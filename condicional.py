0a334d36ced8df66acc287d3d04acda8c4ca70d4
Gabi Falcão <129765553+gabifalcao@users.noreply.github.com>
2025-11-17 16:29:11 -0300

    Add files via upload
    (tão simples demais)

---
+++ condicional.py
@@ -0,0 +1,10 @@
+entrada = input('Você quer entrar ou sair?')
+
+if entrada == 'Entrar':
+    print('Você entrou.')
+    
+elif entrada == "Sair":
+    print('Você saiu.')
+else:
+    print('Você não digitou nenhuma das opções acima.')
+
