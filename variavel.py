c35374e20037ab1d089f65706d73d02054a7dbbc
Gabi Falcão <129765553+gabifalcao@users.noreply.github.com>
2025-11-17 08:58:31 -0300

    Add files via upload

---
+++ variavel.py
@@ -0,0 +1,13 @@
+teste = 1
+print(str (teste) + 'oi')
+teste2 = int('666')
+print(teste2 , type(teste2))
+nome = 'Gabi'
+idade = 20
+maior_de_idade = idade >= 18 
+print('Nome:', nome)
+print('Idade:', idade)
+print('É maior de idade?' , maior_de_idade)
+
+teste3 = 'a' * 10
+print(teste3)
---
+++ variavel2.py
@@ -0,0 +1,13 @@
+nome = 'Gabi'
+sobrenome = 'Falcão'
+idade = 20
+ano_nascimento = 2005
+maior_de_idade = idade >= 18
+altura_em_metros = 1.60
+
+print('Nome:' , nome)
+print('Sobrenome:' , sobrenome)
+print('Idade:' , idade )
+print('Ano de nascimento:' , ano_nascimento)
+print('É maior de idade:' , maior_de_idade)
+print('Altura em metros é:' , altura_em_metros)
